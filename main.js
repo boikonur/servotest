f2791e55443eac6d5b912fbcc2cfd4d36aa75d9d
RADISSON lionel <lionel.radisson@gmail.com>
2017-10-24 17:51:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+addEventListener( 'load' e => {
+    mobileConsole.init();
+} );
+
+document.querySelector( 'button' ).addEventListener( 'click', e => {
+    navigator.bluetooth.requestDevice( {
+            acceptAllDevices: true
+            // filters: [
+            //     name: 'BEE2'
+            // ]
+        } )
+        .then( device => {
+            console.log( device );
+        } )
+        .catch( error => {
+            console.log( error );
+        } )
+} )
