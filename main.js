69ec5a71e5f38a71a55563f319fb7ebb3572ba5e
RADISSON lionel <lionel.radisson@gmail.com>
2017-10-25 13:23:26 +0200

    fixing filters

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ addEventListener( 'load', e => {
 document.querySelector( 'button' ).addEventListener( 'click', e => {
     navigator.bluetooth.requestDevice( {
             // acceptAllDevices: true
-            filters: [
+            filters: [{
                 prefix: 'BEE'
-            ]
+            }]
         } )
         .then( device => {
             console.log( device );
