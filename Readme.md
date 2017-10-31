b49b0f9a5edc14c3486e7f0c2ee1889e7a2d133c
MAKIO135 <makio135@gmail.com>
2017-10-31 10:48:57 +0100

    using BluetoothTerminal

--- Readme.md
+++ Readme.md
@@ -9,6 +9,7 @@ Web Bluetooth API and BLE devices
 - [HM-10 Bluetooth 4 BLE Modules](http://www.martyncurrey.com/hm-10-bluetooth-4ble-modules/)
 - [Control an Arduino via the HM-10 BLE module, from a mobile app on your smartphone](https://evothings.com/control-an-led-using-hm-10-ble-module-an-arduino-and-a-mobile-app/)
 - [Web Bluetooth Terminal](https://github.com/1oginov/Web-Bluetooth-Terminal)
+- [bluetooth-terminal](https://github.com/1oginov/bluetooth-terminal)
 - [Web Bluetooth / Automatic Reconnect Sample](https://googlechrome.github.io/samples/web-bluetooth/automatic-reconnect.html)
 
 ### HM-10 Modules
--- main.js
+++ main.js
@@ -1,66 +1,22 @@
-let bluetoothDevice;
-
 addEventListener('load', e => {
     mobileConsole.init();
-});
 
-document.querySelector('button').addEventListener('click', e => {
-    console.log('click');
+    document.querySelector('button').addEventListener('click', e => {
+        console.log('click');
+        console.log( BluetoothTerminal );
+        // Obtain configured instance
+        let terminal = new BluetoothTerminal();
 
-    bluetoothDevice = null;
-    console.log('Requesting any Bluetooth Device...');
-    navigator.bluetooth.requestDevice({
-        // filters: [...] <- Prefer filters to save energy & show relevant devices.
-        // filters: [ { name: 'CC41-A' } ]
-        acceptAllDevices: true
-    }).then(device => {
-        bluetoothDevice = device;
-        bluetoothDevice.addEventListener('gattserverdisconnected', onDisconnected);
-        connect();
-    }).catch(error => {
-        log('Argh! ' + error);
+        // Override `receive` method to handle incoming data as you want
+        terminal.receive = function(data) {
+            console.log(data);
+        };
+
+        // Request the device for connection and get its name after successful connection
+        terminal.connect().then(() => {
+            console.log(terminal.getDeviceName() + ' is connected!');
+            // Send something to the connected device
+            terminal.send('Simon says: Hello, world!');
+        });
     });
-
-    function connect() {
-        exponentialBackoff(3,
-        /* max retries */
-        2,
-        /* seconds delay */
-        function toTry() {
-            console.log('Connecting to Bluetooth Device... ');
-            return bluetoothDevice.gatt.connect();
-        }, function success() {
-            console.log('> Bluetooth Device connected. Try disconnect it now.');
-
-            bluetoothDevice.writeDataArray(new Uint8Array([0]), '0000ffe0-0000-1000-8000-00805f9b34fb');
-        }, function fail() {
-            console.log('Failed to reconnect.');
-        });
-    }
-
-    function onDisconnected() {
-        console.log('> Bluetooth Device disconnected');
-        connect();
-    }
-
-    /* Utils */
-
-    // This function keeps calling "toTry" until promise resolves or has
-    // retried "max" number of times. First retry has a delay of "delay" seconds.
-    // "success" is called upon success.
-    function exponentialBackoff(max, delay, toTry, success, fail) {
-        toTry().then(result => success(result)).catch(_ => {
-            if (max === 0) {
-                return fail();
-            }
-            console.log('Retrying in ' + delay + 's... (' + max + ' tries left)');
-            setTimeout(function() {
-                exponentialBackoff(--max, delay * 2, toTry, success, fail);
-            }, delay * 1000);
-        });
-    }
-
-    function time(text) {
-        log('[' + new Date().toJSON().substr(11, 8) + '] ' + text);
-    }
-})
+});
